0e7a93ac607876763a10abcee47212e81cfb3c82
derpthebass <ramboxp@live.com>
2013-02-23 16:59:51 -0500

    Update 808.js

--- 808.js
+++ 808.js
@@ -29,10 +29,10 @@ if(autowoot){
 
 if(mirror){
 function voteUpdate(){
-if(API.getUser("50aeb07e96fba52c3ca04ca8").vote === "1"){
+if(API.getUser("50aeb07e96fba52c3ca04ca8").vote = 1){
         $("#button-vote-positive").click();
         console.log("Mirroring 'woot' Vote");
-}else if(API.getUser("50aeb07e96fba52c3ca04ca8").vote  === "-1"){
+}else if(API.getUser("50aeb07e96fba52c3ca04ca8").vote  = -1){
         $("#button-vote-negative").click();
         console.log("Mirroring 'meh' Vote")
         }  
