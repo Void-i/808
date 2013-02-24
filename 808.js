e45722cc8180d8d692cee99a962107037d4da6c1
derpthebass <ramboxp@live.com>
2013-02-23 22:48:06 -0500

    Update 808.js

--- 808.js
+++ 808.js
@@ -13,8 +13,6 @@ var updateChat = function(from, message){
 };
 
 var joined = new Date().getTime();
-
-var day = new Date().getDay;
 /******************************/
 API.addEventListener(API.CHAT, command);
 
@@ -50,14 +48,7 @@ else if(API.getUser("50aeb07e96fba52c3ca04ca8").vote === -1){
         }  
     }
 }
-if(day === 5){
-setInterval(function(){
-if(day === 5 && weird){
-	API.sendChat("/em It's play anything day for the next 24 hours!");
-	weird = false;
-        }
-}, 5000);
-}
+
 /******************************/
 function command(data) {
         if (data.type === "mention" && data.message.indexOf("-debug") > -1 && data.fromID === "50aeb07e96fba52c3ca04ca8" ) {
