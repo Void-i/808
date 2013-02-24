76b27669a4c791dd4e3b7ce9f48b6860727298b3
derpthebass <ramboxp@live.com>
2013-02-23 21:09:31 -0500

    Update 808.js

--- 808.js
+++ 808.js
@@ -107,8 +107,9 @@ function command(data) {
         Models.user.changeDisplayName(RegExp.$1);
         updateChat("[#808] ","Username changed by ,DerpTheBass'");
     }
-       if (data.type === "mention" && data.message.indexOf("-status") > -1 && data.fromID === "50aeb07e96fba52c3ca04ca8" ) {
+       if (data.type === "mention" && data.message.indexOf("-info") > -1 && data.fromID === "50aeb07e96fba52c3ca04ca8" ) {
           API.sendChat("/em has been running for "+elapsed/100000+" minutes."+" Running on "+BrowserDetect.browser+" on"+BrowserDetect.OS+".");
+          if(debug){console.log("Sending status/info")}
     }
 }
 
