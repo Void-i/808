301f5724338478fa2f8d8f78dbc3bced6da754e2
derpthebass <ramboxp@live.com>
2013-03-05 11:04:33 -0500

    Update 808.js

--- 808.js
+++ 808.js
@@ -33,13 +33,7 @@ var updateChat = function(from, message){
 
 var joined = new Date().getTime();
 
-var oldDJs = [];
-
-localStorage.setItem("DJS", JSON.stringify(oldDJs));
-
-var stringifiedDJs = localStorage.getItem("DJs");
-
-var storedDJs = JSON.parse(stringifiedDJs);
+var oldDJs = ["1"];
 /******************************/
 API.addEventListener(API.CHAT, command);
 
@@ -76,6 +70,12 @@ setTimeout(function(){Models.user.changeStatus(0)},2000);
     
 API.addEventListener(API.DJ_ADVANCE, newdj);
 function newdj(){
+localStorage.setItem("DJS", JSON.stringify(oldDJs));
+
+var stringifiedDJs = localStorage.getItem("DJs");
+
+var storedDJs = JSON.parse(stringifiedDJs);
+
 	if(storedDJs.indexOf(API.getDJs()[4].id) != -1){
 	oldDJs.push(API.getDJs()[4].id);
 	console.log("[#808] New DJ");
