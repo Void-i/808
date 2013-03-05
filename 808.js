70414fd22f0bff20205641db091b20d226c0f27b
derpthebass <ramboxp@live.com>
2013-03-05 11:30:39 -0500

    Update 808.js

--- 808.js
+++ 808.js
@@ -33,7 +33,6 @@ var updateChat = function(from, message){
 
 var joined = new Date().getTime();
 
-var oldDJs = ["1"];
 /******************************/
 API.addEventListener(API.CHAT, command);
 
@@ -70,13 +69,12 @@ setTimeout(function(){Models.user.changeStatus(0)},2000);
     
 API.addEventListener(API.DJ_ADVANCE, newdj);
 function newdj(){
+var oldDJs = ["1"];
 localStorage.setItem("DJS", JSON.stringify(oldDJs));
-
 var stringifiedDJs = localStorage.getItem("DJs");
-
 var storedDJs = JSON.parse(stringifiedDJs);
 
-	if(oldDJs.indexOf(API.getDJs()[4].id) === -1){
+	if(storedDJs.indexOf(API.getDJs()[4].id) === -1){
 	oldDJs.push(API.getDJs()[4].id);
 	console.log("[#808] New DJ");
 	//API.sendChat("@"+API.getDJs()[4].username+" I've never seen you in the booth before, keep in mind that non-pony songs will be skipped.")
