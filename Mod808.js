237ff674f149c4ee34e23425c7045be66dfe5a34
derpthebass <ramboxp@live.com>
2013-03-27 21:23:48 -0300

    Update Mod808.js

--- Mod808.js
+++ Mod808.js
@@ -1,4 +1,3 @@
-setTimeout(function(){
     //Init
     API.addEventListener(API.CHAT, chat);
     API.addEventListener(API.DJ_ADVANCE, DJ_Advance);
@@ -313,4 +312,4 @@ localStorage.setItem("hasRun", "Yes");
 
     };
     BrowserDetect.init();
-}, 2000);
+
